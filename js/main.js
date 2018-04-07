be7bfec70b54d45bb51a826b5dcf31e7eadf9ffa
Eric Li <ericyoungli@gmail.com>
2018-04-07 15:02:04 -0400

    add more metadata

--- js/main.js
+++ js/main.js
@@ -6,14 +6,25 @@ request.onload = function() {
   if (request.status >= 200 && request.status < 400) {
     // Success!
     var data = JSON.parse(request.responseText);
-    data.contents.reverse().forEach(function(c) {
-      var text = c.content_html;
 
-      var div = document.createElement("div");
+    data.contents.forEach(function(c) {
+      var div = document.createElement('div');
+      console.log(c.created_at);
       div.className = 'entry';
-      div.innerHTML = text;
-      document.getElementById('entries').appendChild(div);
-    })
+      if (c.class === 'Text') {
+        var text = c.content_html;
+        div.innerHTML = text + "<p>" + formatDate(new Date(c.created_at)) + "</p>";
+      } else if (c.class === 'Image') {
+        div.innerHTML = '<a href="' + c.image.original.url + '" target="_blank"><img src="' + c.image.display.url + '"></a>';
+      }
+      document.getElementById('entries').insertBefore(div, document.getElementById('entries').childNodes[0]);
+
+      var anchors = document.getElementById('entries').getElementsByTagName('a');
+      for (var i = 0; i < anchors.length; i++){
+        anchors[i].setAttribute('target', '_blank');
+      }
+
+    });
   } else {
     // We reached our target server, but it returned an error
   }
@@ -24,3 +35,18 @@ request.onerror = function() {
 };
 
 request.send();
+
+function formatDate(date) {
+  var monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
+
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+
+  return day + ' ' + monthNames[monthIndex] + ' ' + year;
+}
