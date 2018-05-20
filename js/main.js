1b24f0333f8fba85f37c358b9e7b807c6a48ccee
Eric Li <ericyoungli@gmail.com>
2018-05-19 23:42:22 -0400

    add icon

--- js/main.js
+++ js/main.js
@@ -8,6 +8,12 @@ request.onload = function() {
     document.getElementById('dot-dot-dot').style.display = 'none';
     var data = JSON.parse(request.responseText);
 
+    var lastContributor = data.contents[data.contents.length-1].connected_by_user_slug;
+    if (lastContributor == 'nazli-ercan') {
+      document.getElementById('uc-logo').src = 'img/uc_n.png';
+    } else {
+      document.getElementById('uc-logo').src = 'img/uc_e.png';
+    }
     data.contents.forEach(function(c) {
       var div = document.createElement('div');
 
@@ -16,7 +22,6 @@ request.onload = function() {
         var text = c.content_html;
         div.innerHTML = text + "<p>" + formatDate(new Date(c.created_at)) + "</p>";
       } else if (c.class === 'Image') {
-        console.log(c);
         div.innerHTML = '<a href="' + c.image.original.url + '" target="_blank"><img src="' + c.image.display.url + '"></a><div class="caption">' + c.title + '</div>';
       }
       document.getElementById('entries').insertBefore(div, document.getElementById('entries').childNodes[0]);
